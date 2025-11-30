01846fc14e5062b2d00fc8a612986d8b318cbcca
Joshua Akuma <joshuakuma.ja@gmail.com>
2025-11-30 22:24:02 +0100

    made a few changes

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -17,6 +17,6 @@ class BankAccount:
         
 
     def display_balance(self):
-        print(f"Current Balance: ${self.account_balance}")
+        print(f"Current Balance: ${float(self.account_balance)}")
 
 
