051cfcd22214a5dcba0da8de49c8a718a81e0fc9
Joshua Akuma <joshuakuma.ja@gmail.com>
2025-12-14 21:51:45 +0100

    I had to change the name of the table customers on 2 or 3 of these files

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -18,7 +18,7 @@ CREATE TABLE Authors (
 );
 
 
-CREATE TABLE Customers (
+CREATE TABLE customer (
     customer_id PRIMARY KEY,
     customer_name VARCHAR(215),
     email VARCHAR(215),
@@ -30,7 +30,7 @@ CREATE TABLE Orders (
     order_id PRIMARY KEY,
     customer_id INT,
     order_date DATE,
-    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+    FOREIGN KEY (customer_id) REFERENCES customer(customer_id)
 );
 
 
