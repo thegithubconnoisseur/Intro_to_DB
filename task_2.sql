128d5cb6dae918176ad182dcc5b5eae573cf1388
Joshua Akuma <joshuakuma.ja@gmail.com>
2025-12-14 21:59:02 +0100

    Added task_6.sql

--- task_2.sql
+++ task_2.sql
@@ -16,7 +16,7 @@ CREATE TABLE Authors (
 );
 
 
-CREATE TABLE customer (
+CREATE TABLE Customers(
     customer_id INT PRIMARY KEY,
     customer_name VARCHAR(215),
     email VARCHAR(215),
@@ -28,7 +28,7 @@ CREATE TABLE Orders (
     order_id INT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
-    FOREIGN KEY (customer_id) REFERENCES customer(customer_id)
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
 
